401cf4ff534ff94732b45c92c1bed2653336fbec
Vinicius de Castro Coutinho <viniciuscoutinho_@hotmail.com>
2024-02-15 22:16:09 -0300

    backing

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
 class ContaCorrente:
 
-    def __init__(self,nome,cpf):
+    def __init__(self,nome,cpf, agencia,num_conta):
         self.nome = nome
         self.cpf = cpf
         self.saldo = 0
         self.limite = None
-
-    def consultar_saldo(self):
-        print('Seu saldo atual é de R$ {:,.2f}'.format(self.saldo))
+        self.agencia = agencia
+        self.num_conta = num_conta
+        self.transacoes = []
 
     def depositar(self, valor):
         self.saldo += valor
+        self.transacoes.append(valor, self.saldo, data e hora )
+    def consultar_saldo(self):
+        print('Seu saldo atual é de R${:,.2f}'.format(self.saldo))
 
-    def limite_conta(self):
+    def _limite_conta(self):
         self.limite = -1000
         return self.limite
 
     def sacar_dinheiro(self,valor):
-        if self.saldo - valor < self.limite_conta():
+        if self.saldo - valor < self._limite_conta():
             print('Você não tem saldo sufiiciente para sacar esse valor')
             self.consultar_saldo()
         else:
             self.saldo -= valor
 
+    def consultar_limite_chequeespecial(self):
+        print('Seu limite de chque especial é de {:,.2f}'.format(self._limite_conta()))
+
 
 #programa
-conta_Maguila = ContaCorrente("Maguila","035.687.877-99")
+conta_Maguila = ContaCorrente("Maguila","035.687.877-99", 1234, 23456)
 conta_Maguila.consultar_saldo()
 
 #depositando dinheiro
 conta_Maguila.depositar(20000)
 conta_Maguila.consultar_saldo()
 #sacando dinheiro
-conta_Maguila.sacar_dinheiro(20001)
+conta_Maguila.sacar_dinheiro(20900)
 
 print('Saldo Final')
 conta_Maguila.consultar_saldo()
+conta_Maguila.consultar_limite_chequeespecial()
